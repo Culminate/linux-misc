775c8387ce4ca57c2694b9115246efa75d0d18e5
Culminate <bum033@gmail.com>
2019-06-26 18:22:39 +0500

    add: install and remove options

--- telegram_install.sh
+++ telegram_install.sh
@@ -4,22 +4,66 @@ command -v curl >/dev/null 2>&1 || { echo >&2 "curl it's not installed. Aborting
 command -v wget >/dev/null 2>&1 || { echo >&2 "wget it's not installed. Aborting."; exit 1;}
 command -v jq   >/dev/null 2>&1 || { echo >&2 "jq it's not installed. Aborting."; exit 1;}
 
-apigithub="https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest"
-installpath=/opt/bin/
-tmpath=~/Downloads/
+help() {
+	echo "
+Telegram installer from github official repository
+Usage:
+	options:
+	-i	install telegram
+	-r	remove telegram
+	"
+}
 
-randpath=telegram-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)/
-filepath=$tmpath$randpath
+install() {
+	apigithub="https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest"
+	installpath=/opt/bin/
+	tmpath=~/Downloads/
 
-mkdir $filepath
-cd $filepath
+	randpath=telegram-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)/
+	filepath=$tmpath$randpath
 
-json=$(curl -s $apigithub)
-tpath=$(echo -n $json | jq '.assets[] | select(.label == "Linux 64 bit: Binary") | .browser_download_url' | tr -d '"')
-wget $tpath
+	mkdir $filepath
+	cd $filepath
 
-mkdir -p $installpath
-sudo tar xJf * -C $installpath
-sudo ln -sf $installpath/Telegram/Telegram /usr/local/bin/telegram-desktop
+	json=$(curl -s $apigithub)
+	tpath=$(echo -n $json | jq '.assets[] | select(.label == "Linux 64 bit: Binary") | .browser_download_url' | tr -d '"')
+	wget $tpath
 
-rm -r $filepath
+	mkdir -p $installpath
+	sudo tar xJf * -C $installpath
+	sudo ln -sf $installpath\Telegram/Telegram /usr/local/bin/telegram-desktop
+
+	rm -r $filepath
+	exit 0;
+}
+
+remove() {
+	linkpath=$(command -v telegram-desktop 2>&1)
+	if [ -n $linkpath ]; then
+		echo "Symbolic link telegram-desktop not found. Aborting."
+		exit 1;
+	fi
+	tipath=$(readlink $linkpath)
+	rm -rf $(dirname $tipath)
+	rm -rf $linkpath
+	exit 0;
+}
+
+if [ -n $1 ]; then
+	help
+fi
+
+while [ -n "$1" ]
+do
+case "$1" in
+-i)
+	echo "install"
+	install;;
+-r)
+	echo "remove";
+	remove;;
+*)
+	help
+esac
+shift
+done
