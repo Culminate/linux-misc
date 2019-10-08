fbccca42329d1caa46a01048bb497a9a300af664
Culminate <bum033@gmail.com>
2019-10-08 10:00:25 +0500

    ref: use add_repo script

---
+++ repos/sublime-text.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# https://www.sublimetext.com/docs/3/linux_repositories.html#apt
+
+../add_repo.sh --deb "deb https://download.sublimetext.com/ apt/stable/" --key https://download.sublimetext.com/sublimehq-pub.gpg \
+--debfile sublimetext.list --keyfile sublimetext.gpg
---
+++ repos/virtualbox.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# https://www.virtualbox.org/wiki/Linux_Downloads#Debian-basedLinuxdistributions
+
+../add_repo.sh --deb "deb https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"\
+--key https://www.virtualbox.org/download/oracle_vbox_2016.asc --key https://www.virtualbox.org/download/oracle_vbox.asc \
+--debfile virtualbox.list --keyfile virtualbox.gpg
