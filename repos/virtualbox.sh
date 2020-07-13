8947e5e66365003900cb2ebfeeb9748d7b72231e
Culminate <bum033@gmail.com>
2020-07-13 12:03:24 +0500

    fix names

--- repos/virtualbox.sh
+++ repos/virtualbox.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # https://www.virtualbox.org/wiki/Linux_Downloads#Debian-basedLinuxdistributions
 
-../add_repo.sh --deb "deb https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"\
+../add-repo.sh --deb "deb https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib"\
 	--key https://www.virtualbox.org/download/oracle_vbox_2016.asc --key https://www.virtualbox.org/download/oracle_vbox.asc \
 	--debfile virtualbox.list --keyfile virtualbox.gpg
--- repos/vscode.sh
+++ repos/vscode.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # https://code.visualstudio.com/docs/setup/linux#_debian-and-ubuntu-based-distributions
 
-../add_repo.sh --deb "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" \
+../add-repo.sh --deb "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" \
 	--key https://packages.microsoft.com/keys/microsoft.asc \
 	--debfile vscode.list --keyfile microsoft.gpg
